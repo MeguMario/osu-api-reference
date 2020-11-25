b6c0e4652578e29dcc21215bd341576f57dfd368
MeguMario <44724359+MeguMario@users.noreply.github.com>
2020-11-26 01:28:21 +0700

    initial

---
+++ osu_api_ref/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace osu_api_ref
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("dummy");
+            Console.ReadLine();
+        }
+    }
+}
